ad77f223d6c62743d1080898761b5a25c7627802
alpha1256 <34170740+alpha1256@users.noreply.github.com>
2019-12-21 17:43:10 -0500

    Removed invalid statements

--- main.cpp
+++ main.cpp
@@ -7,19 +7,13 @@ using namespace std;
 
 int main()
 {
-    /*char a='b';
-    a= a + 5;
-    string b;
-    b +=a;
-    cout << b << endl;*/
-    back1:
     int a;
     cout << "Enter 1 for Encoding, 2 for decoding and 3 to exit";
     cin >> a;
     if (a==1)
     {
         encoder();
-        goto back1;
+        main();
     }
     else if (a==2)
     {
@@ -30,7 +24,7 @@ int main()
             DecryptFromFile();
         else if (option1 ==2)
             decryptor();
-        goto back1;
+        main();
     }
     else if (a==3)
     return 0;
